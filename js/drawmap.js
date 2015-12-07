226d64988e5ad44258f93def6bd4b08fb8f2bc7c
DavRet <david.r@appmeisterei.de>
2015-12-07 14:15:07 +0100

    Preview Box added

--- js/drawmap.js
+++ js/drawmap.js
@@ -11,18 +11,20 @@ function drawmap() {
     var lat = 49.0134074;
     var zoom = 10;
 
+
+
     map = new OpenLayers.Map('map', {
         projection: new OpenLayers.Projection("EPSG:900913"),
         displayProjection: new OpenLayers.Projection("EPSG:4326"),
         controls: [
             new OpenLayers.Control.Navigation(),
-            new OpenLayers.Control.LayerSwitcher(),
-            new OpenLayers.Control.PanZoomBar()],
+            new OpenLayers.Control.LayerSwitcher()]
+            /*new OpenLayers.Control.PanZoomBar()],
         maxExtent: new OpenLayers.Bounds(-20037508.34, -20037508.34,
             20037508.34, 20037508.34),
         numZoomLevels: 18,
         maxResolution: 156543,
-        units: 'meters'
+        units: 'meters'*/
     });
 
     layer_mapnik = new OpenLayers.Layer.OSM.Mapnik("Mapnik");
@@ -38,13 +40,25 @@ function drawmap() {
 function addMyMarkers() {
 
     // Popup und Popuptext mit evtl. Grafik
-    var popupbarttacke = "<font color=\"black\"><b>Barttacke</b><p><img src=\"img/barttacke.jpg\" width=\"50\" height=\"50\"></p></font>";
+    var popupbarttacke = "<font color=\"black\"><b>Barttacke</b><p><img class='popupPic' src=\"img/barttacke.jpg\" width=\"50\" height=\"50\"></p></font>";
 
-    var popupmopat = "<font color=\"red\"><b>Mopat</b><p><img src=\"img/mopat.jpg\" width=\"50\" height=\"50\"></p></font>";
+    var popupmopat = "<font color=\"red\"><b>Mopat</b><p><img class='popupPic' src=\"img/mopat.jpg\" width=\"50\" height=\"50\"></p></font>";
     // Position des Markers
     addMarker(layer_markers, 12.101631, 49.0134074, popupbarttacke);
     addMarker(layer_markers, 12.5801538, 48.8777333, popupmopat);
 
+}
+
+function popupClick() {
+    $(".popupPic").on("click", function() {
+        $("#menu-left").show();
+    });
+}
+
+function previewClose() {
+    $("#close-preview").on("click", function () {
+        $("#menu-left").hide();
+    });
 }
 
 function menuItemClick() {
