8a907bcae42db08a3c0f3710e1394852849c93cb
mopat <morczinietz@gmail.com>
2016-02-01 17:02:55 +0100

    parse xmldata und erstellen von datenobjekten

---
+++ src/index/DataObjectFactory.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Patrick on 01.02.2016.
+ */
+function DataObject(newsData) {
+    this.title = newsData.title;
+    this.link = newsData.link;
+    this.pubDate = newsData.pubDate;
+    this.content = newsData.content;
+}
+
+function DataObjectFactory() {
+    DataObjectFactory.prototype.createDataObject = function createDataObject(newsData) {
+        var parentClass = newsData;
+
+        return parentClass
+    }
+}
