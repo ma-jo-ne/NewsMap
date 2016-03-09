64768abf0be1fc6f7890cbbaad30ce0f216d062a
mopat <morczinietz@gmail.com>
2016-03-09 15:15:43 +0100

    locations in db

--- csv_parse.php
+++ csv_parse.php
@@ -6,7 +6,52 @@
  * Time: 19:29
  */
 $file = fopen("locations.csv", "r");
+/*$d = array();
 while (($data = fgetcsv($file)) !== FALSE) {
-    print_r($data);
-    print_r("<br>");
+    //echo(json_encode($data));
+    array_push($d, json_encode($data));
 }
+print_r( json_encode($d));*/
+
+$csvData = file_get_contents("locations.csv");
+$lines = explode(PHP_EOL, $csvData);
+$array = array();
+//print_r($lines);
+for ($i = 0; $i < sizeof($lines); $i++) {
+    //$line = explode("\n", $lines);
+    // $jsonObj = explode(";", $line[1]);
+    $line = $lines[$i];
+    $parsedJsonObj = substr($line, 1, -1);
+    //print_r(substr($jsonObj, 1, -1));
+    //print_r("<br>");
+
+    array_push($array, $parsedJsonObj);
+
+}
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "newsmap";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+//print_r($array);
+for ($i = 0; $i < sizeof($array); $i++) {
+    $decodedArray = $array[$i];
+    for ( $j = 0; $j < sizeof($decodedArray); $j++){
+        $locationsArr = explode(";", $decodedArray[0][9]);
+        print_r($locationsArr);
+    }
+
+    echo("<br>");
+}
+//print_r($array);
+//print_r($array);
+//print_r( substr($array[0][0], 1, -1));
+//echo (explode(";",$array[0][0])[1]);
---
+++ store_locations.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Patrick
+ * Date: 09.03.2016
+ * Time: 14:26
+ */
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "wordpress";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+$sql = "SELECT * from locations";
+
+
+if ($result = $conn->query($sql)) {
+    $rows = array();
+    while ($r = mysqli_fetch_assoc($result)) {
+        $rows[] = $r;
+    }
+
+    /* free result set */
+    $result->close();
+}
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "newsmap";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+for ($i = 0; $i < $rows; $i++) {
+    $location = json_decode($rows[$i]["locationData_HGC"]);
+    $articleId = $location->articleId;
+    $locationData = $location->locations;
+    /* print_r($articleId);
+     print_r("<br>");*/
+    for ($j = 0; $j < sizeof($locationData); $j++) {
+        $currentLocation = $locationData[$j];
+        $lat = $currentLocation->lat;
+        $lon = $currentLocation->lon;
+        $city = $currentLocation->name;
+        $state = $currentLocation->state;
+        $county = $currentLocation->county;
+        $region = $currentLocation->region;
+        $municipality = $currentLocation->municipality;
+        // print_r($currentLocation);
+        /*     print_r($lat);
+                print_r($lon);
+                print_r($municipality);
+                print_r($city);
+                print_r($state);
+                print_r($county);
+                print_r("<br>");*/
+        $insert = "INSERT INTO locations (article_id, lat, lon, city, region, municipality, county, state) VALUES ('$articleId','$lat', '$lon', '$city', '$region', '$municipality', '$county', '$state')";
+        $conn->query($insert);
+    }
+}
+print_r("DONE");
